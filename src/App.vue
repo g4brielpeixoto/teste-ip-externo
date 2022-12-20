d31cecc49465f98267807a0acc51541e3a5e5727
gabrielpeixoto/teksystem <gabrielpeixoto@teksystem.com.br>
2022-12-20 09:23:13 -0300

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+export default {
+  created() {
+    fetch('https://api.ipify.org?format=json')
+      .then(response => response.json())
+      .then(data => this.ip = data.ip)
+  },
+  data() {
+    return {
+      ip: 'Hello Vue!'
+    }
+  },
+}
+</script>
+
+<template>
+  <div>
+    {{ ip }}
+  </div>
+</template>
+
